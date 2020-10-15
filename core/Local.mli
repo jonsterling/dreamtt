9771afb188f4b7135b98c89d633147a6ddddfc11
Jonathan Sterling <jon@jonmsterling.com>
2020-10-14 23:13:09 -0400

    typos

--- core/Local.mli
+++ core/Local.mli
@@ -7,13 +7,16 @@ include Monad.S
 
 
 (** {1 Operations} *)
+
 val get_env : tm Env.t m
 val throw : exn -> 'a m
 
 (** {1 Control operators} *)
+
 val scope : tp -> (tm -> 'a m) -> 'a m
 
 
 (** {1 Runners} *)
+
 val run : tm Env.t -> 'a m -> ('a, exn) Result.t
 val run_exn : tm Env.t -> 'a m -> 'a
